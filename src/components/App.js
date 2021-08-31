324d8861b546557b29814ee097b2b3875f162f93
Natalie Barba <nbarba2010@gmail.com>
2021-08-31 14:45:14 -0700

    Completed assignment

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import EyesOnMe from "./EyesOnMe";
+import Keypad from "./Keypad";
+
+function App() {
+    return (
+        <div>
+            <EyesOnMe />
+            <Keypad />
+        </div>
+    );
+}
+
+export default App
